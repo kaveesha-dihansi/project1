a83491c081528f6c1724bd1b9779a05598c20c96
Kaveesha Dihansi <cst22038@std.uwu.ac.lk>
2024-06-06 23:58:17 +0530

    My 1st step

---
+++ Dog.java
@@ -0,0 +1,13 @@
+public class Dog {
+
+    public void speak() {
+        System.out.println("I am speaking");
+    }
+
+    public static void main(String[] args){
+       Dog  dog1=new Dog();  //initialize
+
+       dog1.speak();
+    }
+
+}
